b4c4c15ffb5fb338824ab71fa5418d74cd821ce0
kylemoely <kylemoely@gmail.com>
2023-05-09 17:51:35 -0500

    Added for loop to load saved events from local storage onto the page in their corresponding hour block

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-let events = Object.keys(localStorage);
+let events = {...localStorage};
 let buttons = document.querySelectorAll(".saveBtn");
 let dayEl = $("#currentDay");
 let timeBlocks = document.querySelectorAll(".time-block");
-console.log(timeBlocks)
-console.log(dayjs());
 
 const saveEvent = (event) => {
   let block = event.target.parentElement;
@@ -45,7 +43,12 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  for(let x=0;x<localStorage.length;x++){
+    let timeID = "#" +  localStorage.key(x);
 
+    let timeblock = $(timeID).children();
+    timeblock[1].value = localStorage.getItem(localStorage.key(x));
+  }
   // TODO: Add code to display the current date in the header of the page.
   dayEl.text(dayjs().format("MMMM DD, YYYY"));
 });
